a5894c7f328a5cc5a424e073f6a1a90a18966bc5
Leslie <leslie.appl@gmail.com>
2020-03-20 15:18:34 +0800

    Loaded and Formatted Weather Data onWeatherVC

---
+++ README.md
@@ -0,0 +1,35 @@
+# Weather
+
+- Using Measurement(value: , unit: ) initializer
+- Using MeasurementFormatter( )
+
+            func loadData() {
+            let selected = AppData.selectedCity
+            let cityData = AppData.citiesData[selected]
+            
+            var temperature = Measurement(value: cityData.temperature, unit: UnitTemperature.celsius)
+            var feels = Measurement(value: cityData.feels, unit: UnitTemperature.celsius)
+            if !AppData.celsius {
+                temperature.convert(to: UnitTemperature.fahrenheit)
+                feels.convert(to: UnitTemperature.fahrenheit)
+            }
+            
+            let precipitation = Measurement(value: cityData.precipitation, unit: UnitLength.centimeters)
+            let wind = Measurement(value: cityData.wind, unit: UnitSpeed.kilometersPerHour)
+            let pressure = Measurement(value: cityData.pressure, unit: UnitPressure.millibars)
+            let visibility = Measurement(value: cityData.visibility, unit: UnitLength.kilometers)
+            
+            let formatter = MeasurementFormatter()
+            formatter.unitStyle = MeasurementFormatter.UnitStyle.short
+            formatter.unitOptions = MeasurementFormatter.UnitOptions.providedUnit
+            
+            temperatureLbl.text = formatter.string(from: temperature)
+            feelsLbl.text = formatter.string(from: feels)
+            precipitationLbl.text = formatter.string(from: precipitation)
+            windLbl.text = formatter.string(from: wind)
+            pressureLbl.text = formatter.string(from: pressure)
+            visibilityLbl.text = formatter.string(from: visibility)
+        }
+
+
+
--- Weather/Controller/WeatherVC.swift
+++ Weather/Controller/WeatherVC.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class WeatherVC: UIViewController {
 
+    @IBOutlet weak var cityLbl: UILabel!
+    @IBOutlet weak var temperatureLbl: UILabel!
+    @IBOutlet weak var precipitationLbl: UILabel!
+    @IBOutlet weak var humidityLbl: UILabel!
+    @IBOutlet weak var windLbl: UILabel!
+    @IBOutlet weak var pressureLbl: UILabel!
+    @IBOutlet weak var visibilityLbl: UILabel!
+    @IBOutlet weak var feelsLbl: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        loadData()
+    }
+    
+    func loadData() {
+        let selected = AppData.selectedCity
+        let cityData = AppData.citiesData[selected]
+        
+        var temperature = Measurement(value: cityData.temperature, unit: UnitTemperature.celsius)
+        var feels = Measurement(value: cityData.feels, unit: UnitTemperature.celsius)
+        if !AppData.celsius {
+            temperature.convert(to: UnitTemperature.fahrenheit)
+            feels.convert(to: UnitTemperature.fahrenheit)
+        }
+        
+        let precipitation = Measurement(value: cityData.precipitation, unit: UnitLength.centimeters)
+        let wind = Measurement(value: cityData.wind, unit: UnitSpeed.kilometersPerHour)
+        let pressure = Measurement(value: cityData.pressure, unit: UnitPressure.millibars)
+        let visibility = Measurement(value: cityData.visibility, unit: UnitLength.kilometers)
+        
+        let formatter = MeasurementFormatter()
+        formatter.unitStyle = MeasurementFormatter.UnitStyle.short
+        formatter.unitOptions = MeasurementFormatter.UnitOptions.providedUnit
+        
+        temperatureLbl.text = formatter.string(from: temperature)
+        feelsLbl.text = formatter.string(from: feels)
+        precipitationLbl.text = formatter.string(from: precipitation)
+        humidityLbl.text = "\(cityData.humidity) %"
+        windLbl.text = formatter.string(from: wind)
+        pressureLbl.text = formatter.string(from: pressure)
+        visibilityLbl.text = formatter.string(from: visibility)
+    }
 }
 
